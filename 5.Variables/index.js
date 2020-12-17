454b6ea481017b1a18983230f73a6fc2b6465ee9
ingsona <ingsona06@gmail.com>
2020-12-17 21:07:13 +0700

    5.variable

---
+++ 5.Variables/index.js
@@ -0,0 +1,25 @@
+// String
+var myName = "My name is Bramah"
+var address = 'Prabumulih' 
+
+// Integer
+var firstNumber = 1
+var secondNumber = 4
+
+// Decimal
+var someDecimal = 3.3
+
+// Boolean
+var isHandsome = true
+
+// Array
+var fruits = ['apel','banana',"melon"]
+
+// Object
+var car = {
+    merek : "honda",
+    type: "beat",
+    cc:125
+}
+
+console.log(car)
