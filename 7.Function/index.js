178410b4b06c682b7be5cf1679ea7a63370c9e3a
ingsona <ingsona06@gmail.com>
2020-12-17 21:32:11 +0700

    7.Function

---
+++ 7.Function/index.js
@@ -0,0 +1,17 @@
+function add(){
+    var one = 1
+    var two = 2
+    return one + two
+}
+
+//contoh pemanggilan function tanpa return
+add()
+
+//pemanggilan dengan return
+console.log(add())
+
+
+var three = 3
+function sub(three){
+    return three-1
+}
