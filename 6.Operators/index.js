1ca28a09bc7e292143c93ebdf5b8fbb30984f2f1
ingsona <ingsona06@gmail.com>
2020-12-17 21:16:44 +0700

    6.Operators

---
+++ 6.Operators/index.js
@@ -0,0 +1,14 @@
+var one = 1
+var two = 2
+
+console.log(one + two)
+console.log(one =+1)
+two += 1
+console.log(two)
+
+var firstName = "Bramah"
+var lastName = "ing sona"
+var fullName = firstName + ' ' + lastName
+console.log(fullName)
+
+// string operator integer = string
