d8d619484376b58b72766f40f6431cad53a8716c
ingsona <ingsona06@gmail.com>
2020-12-18 20:33:01 +0700

    19. array method ii

---
+++ S. Array Method II/slice.js
@@ -0,0 +1,4 @@
+var array1 = [1,2,3,4,5,6,7,8,9]
+
+var array2 = array1.slice(3,6)
+// array2 = [4,5,6]
---
+++ S. Array Method II/splice.js
@@ -0,0 +1,5 @@
+var array = ["dumb","ways","dot","id"]
+
+array.splice(0,0, "sangat","keren") // sangat - keren - dumb - ways - dot -id
+
+console.log(array.join(" - "))
