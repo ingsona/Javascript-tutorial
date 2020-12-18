5d76794013c910d801b8fba347da19d4641a14f0
ingsona <ingsona06@gmail.com>
2020-12-18 13:15:27 +0700

    9.event

---
+++ 9.Event/index.js
@@ -0,0 +1,8 @@
+// function getName(){
+//     alert('bramah ing sona')
+// }
+
+function setName(){
+    var txtname = document.getElementById('txtName')
+    document.getElementById('name').innerHTML = txtname.value
+}
