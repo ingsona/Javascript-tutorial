19b86f60fc6662a8199b2f54a11b02764f4bf70f
ingsona <ingsona06@gmail.com>
2020-12-18 18:39:39 +0700

    15. loop i

---
+++ O. Looping I/forEach.js
@@ -0,0 +1,11 @@
+// var cars = ["buggati","lamborgini","bmw"]
+
+// for(i in cars){
+//     document.write(i+". "+ cars[i]+"<br>")
+// }
+
+var brands = ["gucci","Lv","vans"]
+
+brands.forEach(function(brand){
+    document.write("<p>" + brand +"</p>")
+})
