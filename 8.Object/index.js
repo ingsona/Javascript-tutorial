bfc8ba2d9558bed0391396bcdfc4a363f5cd4939
ingsona <ingsona06@gmail.com>
2020-12-18 12:58:22 +0700

    8.object

---
+++ 8.Object/index.js
@@ -0,0 +1,12 @@
+var person = {
+    firstName: "Bramah",
+    lastName: "Ing Sona",
+    age:28,
+    walk(){
+        return 'i am walking'
+    }
+}
+
+console.log(person.firstName + " " + person.lastName)
+
+console.log(person[firstName])
