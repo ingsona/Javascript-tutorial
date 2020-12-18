58ec7b2d8dc3fd632cc8aee9e155367b53f4680b
ingsona <ingsona06@gmail.com>
2020-12-18 14:03:12 +0700

    11. number method

---
+++ K. Number Method/index.js
@@ -0,0 +1,16 @@
+var txtNumber = 12.345
+
+// toString
+document.getElementById('name').innerHTML = txtNumber.toString()
+
+// toFixed
+document.getElementById('name').innerHTML = txtNumber.toFixed()
+
+//toFIxed
+document.getElementById('name').innerHTML = txtNumber.toFixed(2)
+
+// global method
+document.getElementById('name').innerHTML = Number(txtNumber)
+
+// parseint
+document.getElementById('name').innerHTML = parseInt(txtNumber)
