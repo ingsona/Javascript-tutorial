714415241f67c20f3c33252ac8fc3b5a4af5f210
ingsona <ingsona06@gmail.com>
2020-12-18 14:26:42 +0700

    12.conditional i

---
+++ L. Conditional I/index.js
@@ -0,0 +1,16 @@
+// var totalBelanja = prompt("total belanja ?", 0)
+
+//     if(totalBelanja > 30000){
+//         document.write("<h2> Selamat anda mendapatkan micin</h2>")
+//     }
+
+//     document.write("<p>terima kasih sudah belanja di warung kami</p>")
+
+var password = prompt("Password: ")
+
+    if(password == "dumbways.id"){
+        document.write("<h2>Selamat anda berhasil login</h2>")
+    }else{
+        document.write("<p>password salah</p>")
+    }
+
